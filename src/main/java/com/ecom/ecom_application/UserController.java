238f361822fd2f877097edc122a3872da6a45fa9
Angelie Pahila <angeliegracepahila@gmail.com>
2025-10-04 19:06:15 +0800

    Added Edit User API and simplified the request mapping in user controller

--- src/main/java/com/ecom/ecom_application/UserController.java
+++ src/main/java/com/ecom/ecom_application/UserController.java
@@ -9,32 +9,37 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService = new UserService();
 
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
-    @PostMapping("/api/createUser")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user){
         userService.addUser(user);
         return ResponseEntity.ok("User created successfully");
     }
 
-    @GetMapping("/api/users/{userId}")
+    @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable Long userId){
         return userService.fetchUserById(userId)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/api/users/{userId}")
+    @PutMapping("/{userId}")
     public ResponseEntity<String> editUser(@PathVariable Long userId, @RequestBody User user){
-
+       boolean userUpdated = userService.updateUser(userId, user);
+        if(userUpdated){
+            return ResponseEntity.ok("User updated successfully");
+        }
+        return ResponseEntity.notFound().build();
     }
 
 
